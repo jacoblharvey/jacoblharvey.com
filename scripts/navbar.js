e78d7bdde4680b5cf8ecf6631d79293512ee52e3
Jacob Harvey <jacoblharvey@outlook.com>
2025-02-28 12:15:26 -0600

    Isolate navbar styles; Add script for dropdown

---
+++ scripts/navbar.js
@@ -0,0 +1,29 @@
+const dropdownButton = document.getElementById("dropdown-button");
+const dropdownContent = document.getElementById("dropdown-content");
+const toggleArrow = document.getElementById("dropdown-arrow");
+
+// Toggle dropdown function
+const toggleDropdown = () => {
+  dropdownContent.classList.toggle("show");
+  toggleArrow.classList.toggle("dropdown-arrow");
+};
+
+// Toggle dropdown open/close when dropdown button is clicked
+dropdownButton.addEventListener("click", (event) => {
+  event.stopPropagation();
+  toggleDropdown();
+});
+
+// Close dropdown when dom element is clicked
+document.documentElement.addEventListener("click", () => {
+  if (dropdownContent.classList.contains("show")) {
+    toggleDropdown();
+  }
+});
+
+// Close dropdown when esc is clicked
+document.documentElement.addEventListener("keydown", (event) => {
+  if (event.key === "Escape" && dropdownContent.classList.contains("show")) {
+    toggleDropdown();
+  }
+});
